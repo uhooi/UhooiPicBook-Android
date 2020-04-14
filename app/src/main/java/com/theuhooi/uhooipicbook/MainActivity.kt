bd9010906e358cffce5037d47bd69309f41bf645
uhooi <uhooi@server.fake>
2020-04-14 15:39:00 +0900

    Add RecyclerView

--- app/src/main/java/com/theuhooi/uhooipicbook/MainActivity.kt
+++ app/src/main/java/com/theuhooi/uhooipicbook/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.theuhooi.uhooipicbook
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.theuhooi.uhooipicbook.monsterlist.MonsterListFragment
+import com.theuhooi.uhooipicbook.monsterlist.dummy.DummyContent
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), MonsterListFragment.OnListFragmentInteractionListener {
+
+    // MARK: View Life-Cycle Methods
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    override fun onListFragmentInteraction(item: DummyContent.DummyItem?) {
+    }
+
 }
