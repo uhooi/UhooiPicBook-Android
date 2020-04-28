c8fac2f77a44e01ff7b58812007c4273f949dd7a
uhooi <uhooi@server.fake>
2020-04-28 10:27:33 +0900

    Move ViewHolder

--- app/src/main/java/com/theuhooi/uhooipicbook/modules/monsterlist/MonsterListRecyclerViewAdapter.kt
+++ app/src/main/java/com/theuhooi/uhooipicbook/modules/monsterlist/MonsterListRecyclerViewAdapter.kt
@@ -3,16 +3,19 @@ package com.theuhooi.uhooipicbook.modules.monsterlist
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import coil.api.load
 import com.theuhooi.uhooipicbook.R
 import com.theuhooi.uhooipicbook.modules.monsterlist.MonsterListFragment.OnListFragmentInteractionListener
 import com.theuhooi.uhooipicbook.modules.monsterlist.entity.MonsterContent.MonsterItem
+import kotlinx.android.synthetic.main.item_monster_list.view.*
 
 class MonsterListRecyclerViewAdapter(
     private val monsters: List<MonsterItem>,
     private val listener: OnListFragmentInteractionListener?
-) : RecyclerView.Adapter<MonsterListRecyclerViewHolder>() {
+) : RecyclerView.Adapter<MonsterListRecyclerViewAdapter.MonsterListRecyclerViewHolder>() {
 
     // region Stored Instance Properties
 
@@ -41,6 +44,15 @@ class MonsterListRecyclerViewAdapter(
     }
 
     override fun getItemCount(): Int = this.monsters.size
+
+    // endregion
+
+    // region ViewHolder
+
+    class MonsterListRecyclerViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
+        val iconImageView: ImageView = view.icon_imageview
+        val nameTextView: TextView = view.name_textview
+    }
 
     // endregion
 
