8038a79f6d024f9be381d83dc2abeebfd9aa2a99
uhooi <uhooi@server.fake>
2020-05-01 18:18:20 +0900

    Change the color of ActionBar and StatusBar to the theme color of the monster

--- app/src/main/java/com/theuhooi/uhooipicbook/MainActivity.kt
+++ app/src/main/java/com/theuhooi/uhooipicbook/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.theuhooi.uhooipicbook
 
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.core.graphics.ColorUtils
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
@@ -30,6 +34,14 @@ class MainActivity : AppCompatActivity(), MonsterListFragment.OnListFragmentInte
         setSupportActionBar(findViewById(R.id.toolbar))
 
         val navController = findNavController(R.id.nav_host_fragment)
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            if (destination.id == R.id.monster_list_fragment) {
+                this.supportActionBar?.setBackgroundDrawable(
+                    ColorDrawable(ContextCompat.getColor(this, R.color.colorPrimary))
+                )
+                this.window.statusBarColor = ContextCompat.getColor(this, R.color.colorPrimaryDark)
+            }
+        }
         val appBarConfiguration = AppBarConfiguration(navController.graph)
         setupActionBarWithNavController(navController, appBarConfiguration)
     }
@@ -41,6 +53,16 @@ class MainActivity : AppCompatActivity(), MonsterListFragment.OnListFragmentInte
     override fun onListFragmentInteraction(item: MonsterItem) {
         val action = MonsterListFragmentDirections.actionListToDetail(item)
         findNavController(R.id.nav_host_fragment).navigate(action)
+
+        if (item.baseColorCode.isNotEmpty()) {
+            this.supportActionBar?.setBackgroundDrawable(ColorDrawable(Color.parseColor(item.baseColorCode)))
+
+            var hsl = FloatArray(3)
+            ColorUtils.colorToHSL(Color.parseColor(item.baseColorCode), hsl)
+            hsl.set(0, hsl[0] - 6) // TODO: 0未満にならないようにする
+            hsl.set(2, hsl[2] - 0.09f) // TODO: 0未満にならないようにする
+            this.window.statusBarColor = ColorUtils.HSLToColor(hsl)
+        }
     }
 
     // endregion
