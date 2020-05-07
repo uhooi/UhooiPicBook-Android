a3db33a980a0e55f9f5dfadbc764f2a1af7982aa
uhooi <uhooi@server.fake>
2020-05-07 14:57:24 +0900

    Refactor

--- app/src/main/java/com/theuhooi/uhooipicbook/extensions/Int+Color.kt
+++ app/src/main/java/com/theuhooi/uhooipicbook/extensions/Int+Color.kt
@@ -3,9 +3,9 @@ package com.theuhooi.uhooipicbook.extensions
 import androidx.core.graphics.ColorUtils
 
 fun Int.actionBarColorToStatusBarColor(): Int {
-    var hsl = FloatArray(3)
+    val hsl = FloatArray(3)
     ColorUtils.colorToHSL(this, hsl)
-    hsl.set(0, if (hsl[0] < 6) 0f else hsl[0] - 6)
-    hsl.set(2, if (hsl[2] < 0.09f) 0f else hsl[2] - 0.09f)
+    hsl[0] = if (hsl[0] < 6) 0f else hsl[0] - 6
+    hsl[2] = if (hsl[2] < 0.09f) 0f else hsl[2] - 0.09f
     return ColorUtils.HSLToColor(hsl)
 }
