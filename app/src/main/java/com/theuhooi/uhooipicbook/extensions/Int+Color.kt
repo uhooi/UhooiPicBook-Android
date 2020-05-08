e1f572dd196b852577126101797ea32ab59ca297
uhooi <uhooi@server.fake>
2020-05-08 16:22:21 +0900

    Fix lowercase to uppercase

--- app/src/main/java/com/theuhooi/uhooipicbook/extensions/Int+Color.kt
+++ app/src/main/java/com/theuhooi/uhooipicbook/extensions/Int+Color.kt
@@ -5,7 +5,7 @@ import androidx.core.graphics.ColorUtils
 fun Int.actionBarColorToStatusBarColor(): Int {
     val hsl = FloatArray(3)
     ColorUtils.colorToHSL(this, hsl)
-    hsl[0] = if (hsl[0] < 6) 0f else hsl[0] - 6
-    hsl[2] = if (hsl[2] < 0.09f) 0f else hsl[2] - 0.09f
+    hsl[0] = if (hsl[0] < 6) 0F else hsl[0] - 6
+    hsl[2] = if (hsl[2] < 0.09F) 0F else hsl[2] - 0.09F
     return ColorUtils.HSLToColor(hsl)
 }
