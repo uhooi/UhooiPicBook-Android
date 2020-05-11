a8c8c2da8ee7e66e16bb982da35362b8a320f4bb
日髙 尚輝 <naoki.h@arsaga.jp>
2020-05-12 01:42:40 +0900

    Add ViewModel

---
+++ app/src/main/java/com/theuhooi/uhooipicbook/modules/monsterlist/viewmodel/MonsterListViewModel.kt
@@ -0,0 +1,27 @@
+package com.theuhooi.uhooipicbook.modules.monsterlist.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.theuhooi.uhooipicbook.modules.monsterlist.MonstersRepository
+import com.theuhooi.uhooipicbook.modules.monsterlist.entities.MonsterItem
+import com.theuhooi.uhooipicbook.repository.monsters.firebase.MonstersFirestoreClient
+
+class MonsterListViewModel : ViewModel() {
+
+    private val repository: MonstersRepository = MonstersFirestoreClient()
+
+    private val _monsterList = MutableLiveData<List<MonsterItem>>()
+    val monsterList: LiveData<List<MonsterItem>> = _monsterList
+
+    fun loadMonsters() {
+        repository.loadMonsters(
+            onSuccess = {monsters ->
+                _monsterList.value = monsters
+            },
+            onFailure = {
+                //TODO エラーハンドリング
+            }
+        )
+    }
+}
