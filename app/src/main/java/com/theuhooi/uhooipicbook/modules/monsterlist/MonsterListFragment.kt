9ab75e0de20635a8a69d9a52d53b169b79e08526
Naoki-Hidaka <62977464+Naoki-Hidaka@users.noreply.github.com>
2020-05-13 21:37:43 +0900

    Update app/src/main/java/com/theuhooi/uhooipicbook/modules/monsterlist/MonsterListFragment.kt
    Co-authored-by: uhooi <the_uhooi@yahoo.co.jp>

--- app/src/main/java/com/theuhooi/uhooipicbook/modules/monsterlist/MonsterListFragment.kt
+++ app/src/main/java/com/theuhooi/uhooipicbook/modules/monsterlist/MonsterListFragment.kt
@@ -6,8 +6,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.activityViewModels
-import androidx.fragment.app.viewModels
 import androidx.navigation.navGraphViewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.theuhooi.uhooipicbook.R
