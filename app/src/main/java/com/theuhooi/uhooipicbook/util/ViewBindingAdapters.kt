4e8c6a10f082a642209310e5db1d0cbf00c727b8
uhooi <uhooi@server.fake>
2020-05-22 14:26:48 +0900

    Move load BindingAdapter

--- app/src/main/java/com/theuhooi/uhooipicbook/util/ViewBindingAdapters.kt
+++ app/src/main/java/com/theuhooi/uhooipicbook/util/ViewBindingAdapters.kt
@@ -3,9 +3,16 @@ package com.theuhooi.uhooipicbook.util
 import android.view.View
 import android.view.View.GONE
 import android.view.View.VISIBLE
+import android.widget.ImageView
 import androidx.databinding.BindingAdapter
+import coil.api.load
 
 @BindingAdapter("goneUnless")
 fun goneUnless(view: View, visible: Boolean) {
     view.visibility = if (visible) VISIBLE else GONE
+}
+
+@BindingAdapter("imageUrl")
+fun load(imageView: ImageView, imageUrl: String) {
+    imageView.load(imageUrl)
 }
