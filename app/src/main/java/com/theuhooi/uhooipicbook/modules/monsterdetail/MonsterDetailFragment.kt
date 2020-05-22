95f99450b0138faf3cc3fa756a731f5b28eb2ba6
Naoki-Hidaka <naoki.h@arsaga.jp>
2020-05-23 00:22:07 +0900

    Support DataBinding and ViewPager2

--- app/src/main/java/com/theuhooi/uhooipicbook/modules/monsterdetail/MonsterDetailFragment.kt
+++ app/src/main/java/com/theuhooi/uhooipicbook/modules/monsterdetail/MonsterDetailFragment.kt
@@ -6,8 +6,15 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
+import androidx.navigation.navGraphViewModels
+import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager2.widget.ViewPager2
 import coil.api.load
+import com.google.android.material.tabs.TabLayoutMediator
 import com.theuhooi.uhooipicbook.R
+import com.theuhooi.uhooipicbook.databinding.FragmentMonsterDetailBinding
+import com.theuhooi.uhooipicbook.databinding.ItemMonsterDetailBinding
+import com.theuhooi.uhooipicbook.modules.monsterlist.viewmodel.MonsterListViewModel
 import kotlinx.android.synthetic.main.fragment_monster_detail.view.*
 
 class MonsterDetailFragment : Fragment() {
@@ -16,6 +23,8 @@ class MonsterDetailFragment : Fragment() {
 
     private val args: MonsterDetailFragmentArgs by navArgs()
 
+    private val viewModel: MonsterListViewModel by navGraphViewModels(R.id.nav_graph)
+
     // endregion
 
     // region View Life-Cycle Methods
@@ -23,18 +32,50 @@ class MonsterDetailFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_monster_detail, container, false)
+    ): View? = FragmentMonsterDetailBinding.inflate(inflater, container, false).let {
+        it.monsterViewPager.apply {
+            adapter = MonsterDetailAdapter()
+            orientation = ViewPager2.ORIENTATION_HORIZONTAL
+
+            TabLayoutMediator(it.tabLayout, this, false, true) { _, _ ->
+                // do nothing
+            }.attach()
+
+            setPageTransformer { view, position ->
+                view.translationX = position * 100
+            }
+
+            setCurrentItem(viewModel.selectedMonsterItemPosition.value ?: 0, false)
+        }
+        it.lifecycleOwner = viewLifecycleOwner
+        it.root
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+    // endregion
 
-        view.icon_imageview.load(this.args.monster.iconUrlString)
-        view.name_textview.text = this.args.monster.name
-        // TODO: 文字列の加工を終わらせた状態で渡す
-        view.description_textview.text = this.args.monster.description.replace("\\n", "\n")
+    // region RecyclerView Adapter
+
+    private inner class MonsterDetailAdapter : RecyclerView.Adapter<MonsterDetailViewHolder>() {
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MonsterDetailViewHolder = ItemMonsterDetailBinding.inflate(
+            LayoutInflater.from(context), parent, false).let {
+            MonsterDetailViewHolder(it)
+        }
+
+        override fun onBindViewHolder(holder: MonsterDetailViewHolder, position: Int) {
+            holder.binding.also {
+                it.monsterItem = viewModel.monsterList.value?.get(position)
+                it.lifecycleOwner = viewLifecycleOwner
+            }
+        }
+
+        override fun getItemCount(): Int = viewModel.monsterList.value?.size ?: 0
     }
+
+    // endregion
+
+    // region ViewHolder
+
+    private inner class MonsterDetailViewHolder(val binding: ItemMonsterDetailBinding): RecyclerView.ViewHolder(binding.root)
 
     // endregion
 
