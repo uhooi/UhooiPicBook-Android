b05d9c3d2d85b1dfff8a62bc0add9bf3126abd31
Naoki-Hidaka <naoki.h@arsaga.jp>
2020-05-23 15:16:05 +0900

    Toolbarの色を変えるのにMonsterDetailFragmentでもOnListFragmentInteractionを使うので引っ越し

---
+++ app/src/main/java/com/theuhooi/uhooipicbook/util/OnListFragmentInteractionListener.kt
@@ -0,0 +1,7 @@
+package com.theuhooi.uhooipicbook.util
+
+import com.theuhooi.uhooipicbook.modules.monsterlist.entities.MonsterItem
+
+interface OnListFragmentInteractionListener {
+    fun onListFragmentInteraction(item: MonsterItem)
+}
