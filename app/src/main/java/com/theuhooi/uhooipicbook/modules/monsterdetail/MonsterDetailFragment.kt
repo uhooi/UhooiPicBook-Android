04e747bfce4dc449fe0e7e5288460210e047ffd9
uhooi <uhooi@server.fake>
2020-10-02 00:34:50 +0900

    Share monster icon

--- app/src/main/java/com/theuhooi/uhooipicbook/modules/monsterdetail/MonsterDetailFragment.kt
+++ app/src/main/java/com/theuhooi/uhooipicbook/modules/monsterdetail/MonsterDetailFragment.kt
@@ -1,5 +1,8 @@
 package com.theuhooi.uhooipicbook.modules.monsterdetail
 
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.Menu
@@ -7,11 +10,16 @@ import android.view.MenuInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.app.ShareCompat
+import androidx.core.content.FileProvider
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
+import coil.ImageLoader
 import coil.api.load
+import coil.request.ImageRequest
 import com.theuhooi.uhooipicbook.R
 import kotlinx.android.synthetic.main.fragment_monster_detail.view.*
+import java.io.File
+import java.io.FileOutputStream
 
 class MonsterDetailFragment : Fragment() {
 
@@ -58,14 +66,39 @@ class MonsterDetailFragment : Fragment() {
 
     private fun shareMonster() {
         val monster = this.args.monster
-        val text =
-            monster.name + "\n" + monster.description.replace("\\n", "\n") + "\n#UhooiPicBook"
-        ShareCompat.IntentBuilder
-            .from(requireActivity())
-            .setText(text)
-            .setType("text/plain")
-            .setChooserTitle(R.string.share_menu_item_title)
-            .startChooser()
+        val context = requireContext()
+        val request = ImageRequest.Builder(context)
+            .data(monster.iconUrlString)
+            .target { drawable ->
+                val file = File(context.externalCacheDir, "share_temp.jpeg")
+                FileOutputStream(file).use { outputStream ->
+                    val bitmap = (drawable as BitmapDrawable).bitmap
+                    bitmap.compress(Bitmap.CompressFormat.PNG, 100, outputStream)
+                    outputStream.flush()
+                }
+                val fileUri: Uri? = try {
+                    FileProvider.getUriForFile(context, context.packageName + ".fileprovider", file)
+                } catch (e: IllegalArgumentException) {
+                    null
+                }
+                fileUri ?: run {
+                    // TODO: エラーハンドリング
+                }
+                val text =
+                    monster.name + "\n" + monster.description.replace(
+                        "\\n",
+                        "\n"
+                    ) + "\n#UhooiPicBook"
+                ShareCompat.IntentBuilder
+                    .from(requireActivity())
+                    .setText(text)
+                    .setStream(fileUri)
+                    .setType("image/png")
+                    .setChooserTitle(R.string.share_menu_item_title)
+                    .startChooser()
+            }
+            .build()
+        val disposable = ImageLoader(requireContext()).enqueue(request)
     }
 
     // endregion
