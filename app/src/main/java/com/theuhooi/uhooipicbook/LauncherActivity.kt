5c81d1652378f23307d64ff839aa596c8ce290cd
Girum.A <galemu00@gmail.com>
2020-10-22 12:53:43 -0400

    added splash screen and made MainActivity exported=false

---
+++ app/src/main/java/com/theuhooi/uhooipicbook/LauncherActivity.kt
@@ -0,0 +1,22 @@
+package com.theuhooi.uhooipicbook
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class LauncherActivity : AppCompatActivity() {
+
+    private val TIME_OUT = 2000L
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_launcher)
+
+        Handler().postDelayed({
+            val intent = Intent(this@LauncherActivity, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, TIME_OUT)
+    }
+}
