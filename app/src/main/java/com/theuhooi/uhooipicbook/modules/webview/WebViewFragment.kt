3fbaee110004ae13e8c42df918ffdbbe0499d306
417-72KI <417.72ki@gmail.com>
2021-04-14 00:29:22 +0900

    override `shouldOverrideUrlLoading` on `WebVliewClient` https://stackoverflow.com/questions/57685214/google-forum-short-url-not-working-in-android-app-webview

--- app/src/main/java/com/theuhooi/uhooipicbook/modules/webview/WebViewFragment.kt
+++ app/src/main/java/com/theuhooi/uhooipicbook/modules/webview/WebViewFragment.kt
@@ -1,15 +1,19 @@
 package com.theuhooi.uhooipicbook.modules.webview
 
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.webkit.WebChromeClient
+import android.webkit.WebResourceRequest
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
 import com.theuhooi.uhooipicbook.R
+
 
 class WebViewFragment : Fragment() {
 
@@ -36,7 +40,59 @@ class WebViewFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        // this.webView?.webViewClient = WebViewClient() // FIXME: 付けないと外部ブラウザでリンクが開くが、付けるとエラーになる
+        this.webView?.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(
+                view: WebView?,
+                request: WebResourceRequest?
+            ): Boolean {
+                val url = request?.url.toString() ?: return false
+                return when {
+                    url.startsWith("http:") || url.startsWith("https:") -> false
+                    url.startsWith("intent://") -> {
+                        try {
+                            val context = webView?.context ?: return false
+                            val intent = Intent.parseUri(url, Intent.URI_INTENT_SCHEME)
+                                ?: return false
+
+                            val packageManager: PackageManager = context.packageManager
+                            val info = packageManager.resolveActivity(
+                                intent,
+                                PackageManager.MATCH_DEFAULT_ONLY
+                            )
+                            when {
+                                /* This statement can be omitted if you are not strict about
+                                 * opening the Google form url in WebView & can be opened in an
+                                 * External Browser
+                                 */
+                                intent.scheme == "https" || intent.scheme == "http" -> {
+                                    val fallbackUrl = intent.getStringExtra("browser_fallback_url")
+                                    webView?.loadUrl(fallbackUrl)
+                                    return true
+                                }
+                                info != null -> context.startActivity(intent)
+                                else -> {
+                                    // Call external browser
+                                    val fallbackUrl = intent.getStringExtra("browser_fallback_url")
+                                    val browserIntent = Intent(
+                                        Intent.ACTION_VIEW,
+                                        Uri.parse(fallbackUrl)
+                                    )
+                                    context.startActivity(browserIntent)
+                                }
+                            }
+                            true
+                        } catch (e: Exception) {
+                            false
+                        }
+                    }
+                    else -> {
+                        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+                        webView?.context?.startActivity(intent)
+                        true
+                    }
+                }
+            }
+        }
         this.webView?.loadUrl(this.args.urlString)
     }
 
