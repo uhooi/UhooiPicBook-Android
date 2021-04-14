1c7653d8b9eb14afa04f11eebd09cf3b595c1e2c
uhooi <uhooi@server.fake>
2021-04-14 11:53:37 +0900

    Support redirection

--- app/src/main/java/com/theuhooi/uhooipicbook/modules/webview/WebViewFragment.kt
+++ app/src/main/java/com/theuhooi/uhooipicbook/modules/webview/WebViewFragment.kt
@@ -1,9 +1,13 @@
 package com.theuhooi.uhooipicbook.modules.webview
 
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebResourceRequest
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.fragment.app.Fragment
@@ -35,9 +39,72 @@ class WebViewFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        this.webView?.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(
+                view: WebView?,
+                request: WebResourceRequest?
+            ): Boolean {
+                val urlString = request?.url.toString()
+                if (urlString == null) {
+                    return false
+                }
+
+                return when {
+                    urlString.startsWith("http://") || urlString.startsWith("https://") -> false
+                    urlString.startsWith("intent://") -> {
+                        try {
+                            val context = webView?.context
+                            val intent = Intent.parseUri(urlString, Intent.URI_INTENT_SCHEME)
+                            if (context == null || intent == null) {
+                                return false
+                            }
+
+                            if (intent.scheme == "http" || intent.scheme == "https") {
+                                val fallbackUrl = intent.getStringExtra(
+                                    BROWSER_FALLBACK_URL_EXTRA_NAME
+                                )
+                                webView?.loadUrl(fallbackUrl)
+                                return true
+                            }
+
+                            val info = context.packageManager.resolveActivity(
+                                intent,
+                                PackageManager.MATCH_DEFAULT_ONLY
+                            )
+                            if (info != null) {
+                                context.startActivity(intent)
+                            } else {
+                                val fallbackUrl = intent.getStringExtra(
+                                    BROWSER_FALLBACK_URL_EXTRA_NAME
+                                )
+                                val browserIntent =
+                                    Intent(Intent.ACTION_VIEW, Uri.parse(fallbackUrl))
+                                context.startActivity(browserIntent)
+                            }
+                            true
+                        } catch (e: Exception) {
+                            false
+                        }
+                    }
+                    else -> {
+                        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(urlString))
+                        webView?.context?.startActivity(intent)
+                        true
+                    }
+                }
+            }
+        }
+
         this.webView?.settings?.javaScriptEnabled = true
-        this.webView?.webViewClient = WebViewClient()
         this.webView?.loadUrl(this.args.urlString)
+    }
+
+    // endregion
+
+    // region Companion Object
+
+    companion object {
+        const val BROWSER_FALLBACK_URL_EXTRA_NAME = "browser_fallback_url"
     }
 
     // endregion
