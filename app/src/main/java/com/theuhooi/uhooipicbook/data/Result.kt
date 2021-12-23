9b87ad97e1064f937e228f2dab342fb053107743
uhooi <the_uhooi@yahoo.co.jp>
2021-12-23 15:33:43 +0900

    Remove unused method

--- app/src/main/java/com/theuhooi/uhooipicbook/data/Result.kt
+++ app/src/main/java/com/theuhooi/uhooipicbook/data/Result.kt
@@ -23,7 +23,3 @@ sealed class Result<out R> {
     data class Success<out T>(val data: T) : Result<T>()
     data class Error(val exception: Exception) : Result<Nothing>()
 }
-
-fun <T> Result<T>.successOr(fallback: T): T {
-    return (this as? Result.Success<T>)?.data ?: fallback
-}
